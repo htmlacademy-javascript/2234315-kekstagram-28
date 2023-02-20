073869ab9bb7c1768c463198562c06acac25079f
katya-illarionova <katya.illarionova@travelline.ru>
2023-02-20 23:23:11 +0300

    добавляет functions.js fix

--- js/functions.js
+++ js/functions.js
@@ -5,6 +5,10 @@ function checkCommentLength(comment, length) {
   return false;
 }
 
+checkCommentLength('проверяемая строка', 20);
+checkCommentLength('проверяемая строка', 18);
+checkCommentLength('проверяемая строка', 10);
+
 function checkPalindromm(palindrom) {
   palindrom = palindrom.toLowerCase().replaceAll(' ', '');
   for (let i = 0; i < palindrom.length / 2; i++) {
@@ -15,13 +19,27 @@ function checkPalindromm(palindrom) {
   return true;
 }
 
+checkPalindromm('топот');
+checkPalindromm('ДовОд');
+checkPalindromm('Кекс');
+checkPalindromm('Лёша на полке клопа нашёл ');
+
 function getNumber(message) {
   message = String(message).replaceAll(/[^\d]/g, '');
   const number = parseInt(message, 10);
   return number;
 }
 
-function getFileName(text, length, fillSymbol){
+getNumber('2023 год');
+getNumber('ECMAScript 2022');
+getNumber('1 кефир, 0.5 батона');
+getNumber('агент 007');
+getNumber('а я томат');
+getNumber(2023);
+getNumber(-1);
+getNumber(1.5);
+
+function getFileName(text, length, fillSymbol) {
   if(text.length < length){
     const size = length - text.length;
     let fillText = '';
@@ -37,3 +55,9 @@ function getFileName(text, length, fillSymbol){
   }
   return text;
 }
+
+getFileName('1', 2, '0');
+getFileName('1', 4, '0');
+getFileName('q', 4, 'werty');
+getFileName('q', 4, 'we');
+getFileName('qwerty', 4, '0');
