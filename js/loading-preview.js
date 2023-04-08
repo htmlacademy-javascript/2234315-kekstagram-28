324c295359750c85ac61c137c0cd9279ab65bbff
katya-illarionova <katya.illarionova@travelline.ru>
2023-04-08 17:14:28 +0300

    Фото на память

--- js/loading-preview.js
+++ js/loading-preview.js
@@ -1,13 +1,34 @@
 import {openModal, closeModal} from './loading-modal.js';
+import {showErrorMessage} from './messages.js';
+
+const FILE_TYPES = ['jpg', 'jpeg', 'png'];
 
 const modalElement = document.querySelector('.img-upload__overlay');
 const modalOpenElement = document.querySelector('#upload-file');
 const modalCloseElement = modalElement.querySelector('.img-upload__cancel');
+const preview = document.querySelector('.img-upload__preview img');
+const filtersPreviews = document.querySelectorAll('.effects__preview');
 
 const initLoadingPreview = () => {
   modalOpenElement.addEventListener('change', () => {
+    const file = modalOpenElement.files[0];
+    const fileName = file.name.toLowerCase();
+    const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
+
+    if (!matches) {
+      showErrorMessage();
+
+      return;
+    }
+
+    preview.src = URL.createObjectURL(file);
+
+    for (const filtersPreview of filtersPreviews) {
+      filtersPreview.style.backgroundImage = `url(${preview.src})`;
+    }
     openModal();
   });
+
   modalCloseElement.addEventListener('click', () => {
     closeModal();
   });
